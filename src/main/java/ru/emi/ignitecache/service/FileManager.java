2b1819ab534e81903e45d48a13c162a3fde3a893
Mikhail Epatko <mikhail.epatko@gmail.com>
2020-10-20 16:49:33 +0300

    Closes #5 - 10. Created Apache Ignite config. Fixed logging format.

--- src/main/java/ru/emi/ignitecache/service/FileManager.java
+++ src/main/java/ru/emi/ignitecache/service/FileManager.java
@@ -1,36 +1,23 @@
 package ru.emi.ignitecache.service;
 
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
 import java.io.File;
-
-/**
- * Contains logic of managing local files in application work directory.
- */
 
 @Slf4j
 public class FileManager {
 
-    /**
-     * Create directory if not exists.
-     *
-     * @param path {@link String} path to the directory.
-     * @return {@code true} if directory was created successful or directory already exists, {@code false} if got
-     * Exception.
-     */
     public boolean createIfNotExists(String path) {
         try {
             File dir = new File(path);
             if (!dir.exists()) {
-                log.info("Create directory {}.", dir);
+                log.info("-----> Create directory {}.", dir);
                 return dir.mkdirs();
             } else {
-                log.info("Directory {} already exists.", dir.getPath());
+                log.info("-----> Directory {} already exists.", dir.getPath());
                 return true;
             }
         } catch (Exception e) {
-            log.error("Exception when try check or create directory: {}", path);
+            log.error("-----> Exception when try check or create directory: {}", path);
             return false;
         }
     }
